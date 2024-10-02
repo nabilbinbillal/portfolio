24f535870b9ec8b7792046409583f0722151ae9f
Nabil Bin Billal <nabilbinbilal@gmail.com>
2024-10-02 16:25:23 +0600

    summary

---
+++ _next/static/e1zqgUkBnBDAgC7d_97f1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-f907e58eeb1e5b29.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
